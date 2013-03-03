1f18e5ee902d31da4a6d2859a93ee857a2878056
Jon Austin <jon.i.austin@gmail.com>
2013-03-03 11:15:43 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hckrfeed::Application.config.secret_token = 'f56a982b440380a4594d5a5ba94f30d6e3aa2c69f380287aeb184cd169c26ed3466abde21834938c190ee9ec9f9d4be99bddc6723fe85c97ad2fb46de29245fa'
